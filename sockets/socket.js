053a9ad0b34297f132595ce981b8e5c93a04b765
sebastiansandoval27 <juan.sandovals@usantoto.edu.co>
2021-03-28 21:17:50 -0500

    Sockets finished

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,13 +1,50 @@
-const {io} = require('../index');
+const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
 
-// Socket Messages
+
+const bands = new Bands();
+
+bands.addBand( new Band('Ed Sheeran') );
+bands.addBand( new Band('Santiago Cruz') );
+bands.addBand( new Band('Mana') );
+bands.addBand( new Band('U2') );
+
+
+// Mensajes de Sockets
 io.on('connection', client => {
-  console.log('Connected');
-  client.on('disconnect', () => {
-    console.log('Disconnected');
-  });
-  client.on( 'message', ( payload ) => {
-    console.log('Message:', payload.name);
-    io.emit('message',{ admin:'Message received' });
-  });
+    console.log('Cliente conectado');
+
+    client.emit('active-bands',bands.getBands());
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    client.on('mensaje', ( payload ) => {
+      console.log('Mensaje', payload);
+
+      io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
+    });
+
+    client.on('emit-message', ( payload ) => {
+      /* io.emit( 'new-message', payload ); // Emite a todos */
+      client.broadcast.emit('new-message', payload ); // Emite a todos menos el que lo emitió
+    });
+
+    client.on('vote-band', ( payload ) => {
+      bands.voteBand(payload.id);
+      io.emit('active-bands',bands.getBands());
+    });
+
+    client.on('add-band', ( payload ) => {
+      var band = new Band(payload.name);
+      bands.addBand(band);
+      io.emit('active-bands',bands.getBands());
+    });
+
+    client.on('delete-band', ( payload ) => {
+      bands.deleteBand(payload.id);
+      io.emit('active-bands',bands.getBands());
+    });
 });
